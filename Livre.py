77ed33a3a923f94622e2b4c6ea1c31a0e17b8956
doom <de.simone.dc@gmail.com>
2025-07-29 17:46:34 +0200

    Ajout du constructeur de la classe Livre.

---
+++ Livre.py
@@ -0,0 +1,8 @@
+class Livre:
+    def __init__(self, isbn, date_achat, titre, auteur, editeur, categorie):
+        self.isbn = isbn
+        self.date_achat = date_achat
+        self.titre = titre
+        self.auteur = auteur
+        self.editeur = editeur
+        self. categorie = categorie
