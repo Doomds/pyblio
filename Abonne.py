ee403c016543d8975202daee8a9c95cc8c6ea053
doom <de.simone.dc@gmail.com>
2025-07-29 20:27:41 +0200

    ajout de la fonction str dans la classe Abonne

--- Abonne.py
+++ Abonne.py
@@ -7,3 +7,13 @@ class Abonne:
         self.tel = tel
         self.email = email
         self. date_adhesion = date_adhesion
+    
+    def __str__(self):
+        return f"""ID: {self.id}
+        \nNom: {self.nom}
+        \nPrénom: {self.prenom}
+        \nAdresse: {self.adresse}
+        \nTel: {self.tel}
+        \nEmail: {self.email}
+        \nDate d'adhésion: {self.date_adhesion}
+        \n=================================="""
--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from Abonne import Abonne
 
 livre1 = Livre("9782075100649", "29/7/2025", "Le sorcier de la montagne de feu", "Steve Jackson & Ian Livingstone", "Gallimard Jeunesse", "Défis fantastiques")
 
-
 print(livre1)
-
 
 emprunt1 = Emprunt("01/08/2025", "1", "15/08/2025", None, None)
 
-
 print(emprunt1)
+
+abonne1 = Abonne("1", "Doe", "John", "Rue de la trouille 62", "0478/68.95.44", "john.doe@trouille.com", "01/06/2025")
+
+print(abonne1)
